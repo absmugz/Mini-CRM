ee99e14c54e26599856eea8690c08e09dbda6c15
Absolom Mugwagwa <absolom@Absolom-Mugwagwa.local>
2019-06-24 09:12:24 +0200

    setting up relationships

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $fillable = [
+        'company_id', 'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class, 'company_id');
+    }
 }
