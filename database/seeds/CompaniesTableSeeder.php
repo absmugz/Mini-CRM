8d22a5224f65ec56e2acbe11c6dccfb80a3a06a2
Absolom Mugwagwa <absolom@Absolom-Mugwagwa.local>
2019-06-24 09:49:52 +0200

    setting up seeder tests before faker

--- database/seeds/CompaniesTableSeeder.php
+++ database/seeds/CompaniesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Company;
 
 class CompaniesTableSeeder extends Seeder
 {
@@ -11,6 +12,16 @@ class CompaniesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //'name', 'email', 'logo', 'description', 'url'
+        $c1 = new Company;
+        $c1->name = 'Roam.Africa';
+        $c1->email = 'roam@roam.com';
+        $c1->logo = 'http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/mens-better-than-naked-jacket-AVMH_LC9_hero.png';
+        $c1->description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua consequat.';
+        $c1->url = 'https://roam.africa/';
+
+        $c1->save();
+
     }
 }
